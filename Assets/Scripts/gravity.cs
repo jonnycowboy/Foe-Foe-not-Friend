cae567899cbff1ebeb72722f34c048c475e25879
Jonathan Lussier <jonnycowboy@gmail.com>
2015-03-30 23:38:39 -0400

    Light-based fog of war, satellite flick
    added light to satellite, scaled "flick" of satellite

--- Assets/Scripts/gravity.cs
+++ Assets/Scripts/gravity.cs
@@ -5,9 +5,12 @@ public class gravity : MonoBehaviour {
 	private GameObject planet;
 	public Rigidbody rb;
 	private Vector3 oldMouse;
-	private Vector3 mouseSpeed;
+	private Vector3 oldMouse2;
+	private Vector3 mouseDist;
 	Vector3 offset;
 	Vector3 screenPoint;
+	bool flinged = false;
+	bool dragging = false;
 
 	void Start() {
 		planet = GameObject.Find("ceres");
@@ -15,8 +18,20 @@ public class gravity : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+		float td = 0;
 		Vector3 gravityVector = planet.transform.position - transform.position;
 		rb.AddForce(gravityVector, ForceMode.Acceleration);
+		if (dragging){
+			td = td + Time.fixedDeltaTime;
+			mouseDist = oldMouse - Input.mousePosition;
+		}
+		if (flinged) {
+			//rb.AddForce(Vector3.Scale(mouseDist.normalized, new Vector3(-1/td, -1/td, -1/td)), ForceMode.VelocityChange);
+			rb.AddForce(Vector3.Scale(mouseDist, new Vector3(-1/(Time.fixedDeltaTime*100), -1/(Time.fixedDeltaTime*100), -1/(Time.fixedDeltaTime*100))), ForceMode.VelocityChange);
+			flinged = false;
+			td = 0;
+			dragging = false;
+		}
 
 	}
 
@@ -32,15 +47,19 @@ public class gravity : MonoBehaviour {
 
 	void OnMouseDrag()
 	{
+		dragging = true;
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 		transform.position = curPosition;
+		oldMouse = Input.mousePosition;
 	}
 	
 	void OnMouseUp(){
-		mouseSpeed = oldMouse - Input.mousePosition;
-		//rb.AddForce(mouseSpeed.normalized * -1, ForceMode.Impulse);
-		rb.velocity = mouseSpeed.normalized;
+
+		//mouseDist = oldMouse - Input.mousePosition;
+		flinged = true;
+		//rb.AddForce(mouseSpeed.normalized * -1, ForceMode.VelocityChange);
+		//rb.velocity = mouseSpeed.normalized;
 	}
 
 	// Update is called once per frame
